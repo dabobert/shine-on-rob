f723e8ff9a7b7d004544feffda4a8b42be5ab5c3
Robert Jenkins <bobert@gmail.com>
2018-12-28 22:57:10 -0500

    created te messaes route

--- routes/messages.js
+++ routes/messages.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET message. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('this is a start');
 });
 
 module.exports = router;
