0fd936560355afdd63639b29f083c8519799edea
Robert Jenkins <bobert@gmail.com>
2018-12-30 16:47:20 -0500

    got the state checker working
    spent a bit chasing my tail because i was looking for someting in res
    that was actually in req

--- routes/messages.js
+++ routes/messages.js
@@ -1,14 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var pry = require('pryjs')
+
+
+
 
 /* GET message. */
 router.get('/', function(req, res, next) {
-  if (req.query.input)
-    res.json({
-      value: req.query.input
-    })
-  else
-    res.send('this is a start');
+  res.send('you must "post" a request');
 });
+
+
+router.post('/', function(req, res, next) {
+// eval(pry.it)
+  switch(req.body.state) {
+    case "after_introduction":
+      res.json({ messsage: "let me look you up"});
+      break;
+    case undefined:
+      res.json({ messsage: "I'm confused" });
+      break;
+    default: 
+      res.json({ messsage: 'this the start' })
+  }
+
+
+
+  // if (req.query.input)
+  //   res.json({
+  //     value: req.query.input
+  //   })
+  // else
+  //   res.send('this is a start');
+});
+
+
+
 
 module.exports = router;
