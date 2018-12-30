2cd8855b1472ba36d204c50daf4bf9b04d18e214
Robert Jenkins <bobert@gmail.com>
2018-12-30 18:37:20 -0500

    got rid of dumb pool information in mysql config

--- sequelize.js
+++ sequelize.js
@@ -3,13 +3,7 @@ const UserModel = require('./models/user')
 
 const sequelize = new Sequelize('se_test', 'root', '', {
   host: 'localhost',
-  dialect: 'mysql',
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+  dialect: 'mysql'
 })
 
 
