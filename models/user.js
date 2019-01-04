d41c1478838802bdf9c72902d84e616f95a2a19c
Robert Jenkins <bobert@gmail.com>
2019-01-03 23:13:36 -0500

    change in whitespace

--- models/user.js
+++ models/user.js
@@ -1,11 +1,11 @@
 module.exports = (sequelize, type) => {
-    return sequelize.define('users', {
-        id: {
-          type: type.INTEGER,
-          primaryKey: true,
-          autoIncrement: true
-        },
-        name: type.STRING,
-        goal: type.STRING
-    })
+  return sequelize.define('users', {
+    id: {
+      type: type.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: type.STRING,
+    goal: type.STRING
+  })
 }
