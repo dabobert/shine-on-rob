8347952fce2abccb8696961abe8bfed10384a39f
Robert Jenkins <bobert@gmail.com>
2019-01-06 10:12:01 -0500

    got basis of react tests to run

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+// import { spy } from 'sinon';
+import App from '../App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
