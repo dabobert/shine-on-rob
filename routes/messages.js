b5de69860f615ba98313efe1c5d9a0c42792597f
Robert Jenkins <bobert@gmail.com>
2019-01-06 10:06:44 -0500

    updated comments

--- routes/messages.js
+++ routes/messages.js
@@ -1,7 +1,6 @@
 /*
 To do:
-  convert fields to hidden text fields
-  testing for backend
+  uncomment messages.test
   testing for frontend
 */
 
@@ -9,11 +8,14 @@ To do:
 /*
   Improvements:
     Create a service object for each state, this way a state could respond to multiple inputs]
-    Use a graph DB to store conversatioal pathways, better mapping of conversional flows    Utlize AIML - https://blog.recime.io/using-aiml-and-nlp-to-create-a-conversation-flow-for-your-chatbot-fea63d09b2e6
-    replace promises with async/await functions
+    Use a graph DB to store conversatioal pathways, better mapping of conversional flows
+    Utlize AIML as a way to provide support for nlp tools - https://blog.recime.io/using-aiml-and-nlp-to-create-a-conversation-flow-for-your-chatbot-fea63d09b2e6
     move db config out of the model into a yml file not in the repo or the env variables
     my react throws a warning
     use local db for testing. created name with random uuid for testing in the app
+    Refactor code to use a sync, and chain promised.  In places i nested thens rather than chaining.  this should be refactored to use await or chaining
+    (Assuming a more complex user account system is not being created, where users and goals are not being normalized a la classic dB design)
+    Add created at updated at and counts to users table. No need to duplicate data, can get most recent goal via updated at and can store how many times a goal has been requested via counts
 */
 
 const { User } = require('../sequelize')
